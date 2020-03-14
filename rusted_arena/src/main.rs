258cb6ba4d3abd08c309b5130e8805dc33602153
Orchaldir <orchaldir@gmail.com>
2020-03-14 10:22:58 +0100

    Reformat code

--- rusted_arena/src/main.rs
+++ rusted_arena/src/main.rs
@@ -7,10 +7,10 @@ use render::Renderer;
 
 struct PistonRenderer<'a, 'b> {
     context: Context,
-    graphics: &'a mut G2d<'b>
+    graphics: &'a mut G2d<'b>,
 }
 
-impl<'a,'b> Renderer for PistonRenderer<'a,'b> {
+impl<'a, 'b> Renderer for PistonRenderer<'a, 'b> {
     fn start(&mut self) {
         clear([0.0, 0.0, 0.0, 1.0], self.graphics);
     }
@@ -21,22 +21,25 @@ impl<'a,'b> Renderer for PistonRenderer<'a,'b> {
 }
 
 fn main() {
-    let mut window: PistonWindow =
-        WindowSettings::new("Rusted Arena", [640, 480])
-            .exit_on_esc(true)
-            .resizable(false)
-            .build().unwrap();
+    let mut window: PistonWindow = WindowSettings::new("Rusted Arena", [640, 480])
+        .exit_on_esc(true)
+        .resizable(false)
+        .build()
+        .unwrap();
 
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g, _device| {
-            let mut renderer = PistonRenderer { context: c, graphics: g };
+            let mut renderer = PistonRenderer {
+                context: c,
+                graphics: g,
+            };
 
             render(&mut renderer);
         });
     }
 }
 
-fn render<T: Renderer>(renderer : &mut T) {
+fn render<T: Renderer>(renderer: &mut T) {
     renderer.start();
     renderer.render_rectangle([0.0, 0.0, 100.0, 100.0], [1.0, 0.0, 0.0, 1.0]);
     renderer.render_rectangle([100.0, 0.0, 100.0, 100.0], [0.0, 0.0, 1.0, 1.0]);
--- rusted_arena/src/render/mod.rs
+++ rusted_arena/src/render/mod.rs
@@ -1,4 +1,3 @@
-
 pub trait Renderer {
     fn start(&mut self);
     fn render_rectangle(&mut self, rect: [f64; 4], color: [f32; 4]);
