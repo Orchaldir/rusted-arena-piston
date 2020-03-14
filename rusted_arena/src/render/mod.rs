f6fc515fa92b26ba9b66229c61d22db5425a6fab
Orchaldir <orchaldir@gmail.com>
2020-03-14 22:21:54 +0100

    [Close #10] Add test with mocking

--- rusted_arena/src/render/mod.rs
+++ rusted_arena/src/render/mod.rs
@@ -1,7 +1,9 @@
 pub mod tile;
 
 use crate::math::Point;
+use mockall::*;
 
+#[automock]
 pub trait Renderer {
     fn start(&mut self);
     fn render_rectangle(&mut self, pos: &Point, size: &Point, color: [f32; 4]);
--- rusted_arena/src/render/tile.rs
+++ rusted_arena/src/render/tile.rs
@@ -16,3 +16,36 @@ impl TileRenderer {
         renderer.render_rectangle(&pos, &self.tile_size, color);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::MockRenderer;
+    use super::*;
+
+    #[test]
+    fn test_render_full() {
+        let start = Point { x: 1, y: 2 };
+        let size = Point { x: 15, y: 25 };
+        let pos = Point { x: 3, y: 4 };
+        let color = [0.1, 0.2, 0.3, 0.4];
+
+        let mut mock = MockRenderer::new();
+        mock.expect_render_rectangle()
+            .times(1)
+            .withf(|p: &Point, s: &Point, c: &[f32; 4]| {
+                p.x == 46
+                    && p.y == 102
+                    && s.x == 15
+                    && s.y == 25
+                    && c[0] == 0.1
+                    && c[1] == 0.2
+                    && c[2] == 0.3
+                    && c[3] == 0.4
+            })
+            .return_const(());
+
+        let tile_renderer = TileRenderer::new(start, size);
+
+        tile_renderer.render_full(&mut mock, &pos, color);
+    }
+}
