ad7bc49111b02dc4e4d51af5c21c44f0450d45ed
Orchaldir <orchaldir@gmail.com>
2020-03-14 09:37:11 +0100

    [#3] Add Renderer

--- rusted_arena/src/main.rs
+++ rusted_arena/src/main.rs
@@ -2,19 +2,35 @@ extern crate piston_window;
 
 use piston_window::*;
 
+struct Renderer<'a, 'b> {
+    context: Context,
+    graphics: &'a mut G2d<'b>
+}
+
+impl<'a,'b> Renderer<'a,'b> {
+    fn start(&mut self) {
+        clear([0.0, 0.0, 0.0, 1.0], self.graphics);
+    }
+
+    fn render_rectangle(&mut self, rect: [f64; 4], color: [f32; 4]) {
+        rectangle(color, rect, self.context.transform, self.graphics);
+    }
+}
+
 fn main() {
     let mut window: PistonWindow =
         WindowSettings::new("Rusted Arena", [640, 480])
-            .exit_on_esc(true).build().unwrap();
+            .exit_on_esc(true)
+            .resizable(false)
+            .build().unwrap();
+
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g, _device| {
-            clear([1.0; 4], g);
-            rectangle([1.0, 0.0, 0.0, 1.0],
-                      [0.0, 0.0, 100.0, 100.0],
-                      c.transform, g);
-            rectangle([0.0, 0.0, 1.0, 1.0],
-                      [100.0, 0.0, 100.0, 100.0],
-                      c.transform, g);
+            let mut renderer = Renderer { context: c, graphics: g };
+
+            renderer.start();
+            renderer.render_rectangle([0.0, 0.0, 100.0, 100.0], [1.0, 0.0, 0.0, 1.0]);
+            renderer.render_rectangle([100.0, 0.0, 100.0, 100.0], [0.0, 0.0, 1.0, 1.0]);
         });
     }
 }
