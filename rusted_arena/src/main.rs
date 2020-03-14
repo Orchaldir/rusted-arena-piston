9f98a10b757c553afb32f7b9ab49e6fc536b7654
Orchaldir <orchaldir@gmail.com>
2020-03-14 08:23:16 +0100

    [#2] Add Piston demo

--- rusted_arena/src/main.rs
+++ rusted_arena/src/main.rs
@@ -1,3 +1,20 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow =
+        WindowSettings::new("Rusted Arena", [640, 480])
+            .exit_on_esc(true).build().unwrap();
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g, _device| {
+            clear([1.0; 4], g);
+            rectangle([1.0, 0.0, 0.0, 1.0],
+                      [0.0, 0.0, 100.0, 100.0],
+                      c.transform, g);
+            rectangle([0.0, 0.0, 1.0, 1.0],
+                      [100.0, 0.0, 100.0, 100.0],
+                      c.transform, g);
+        });
+    }
 }
