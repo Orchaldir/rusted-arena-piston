1c7178db6d431efa27141eed51dc398577049b76
Orchaldir <orchaldir@gmail.com>
2020-03-15 08:43:47 +0100

    [#4] Render a single character centered

--- rusted_arena/src/main.rs
+++ rusted_arena/src/main.rs
@@ -6,6 +6,7 @@ extern crate piston_window;
 mod math;
 mod render;
 
+use crate::piston_window::character::CharacterCache;
 use math::Point;
 use piston_window::*;
 use render::tile::TileRenderer;
@@ -24,15 +25,18 @@ impl<'a, 'b> Renderer for PistonRenderer<'a, 'b> {
     }
 
     fn render_char(&mut self, c: char, pos: &Point, size: u32, color: [f32; 4]) {
-        text::Text::new_color(color, size)
-            .draw(
-                &c.to_string(),
-                self.glyphs,
-                &self.context.draw_state,
-                self.context.transform.trans(pos.x as f64, pos.y as f64),
-                self.graphics,
-            )
-            .unwrap();
+        let image = Image::new_color(color);
+        let character = self.glyphs.character(size, c).unwrap();
+        let center = pos + (size / 2) as i32;
+        let start_x = center.x as f64 - character.advance_size[0] * 0.5;
+        let start_y = center.y as f64 - character.offset[1] * 0.5;
+
+        image.draw(
+            character.texture,
+            &self.context.draw_state,
+            self.context.transform.trans(start_x, start_y),
+            self.graphics,
+        );
     }
 
     fn render_rectangle(&mut self, pos: &Point, size: &Point, color: [f32; 4]) {
@@ -79,5 +83,5 @@ fn render<R: Renderer>(renderer: &mut R, tile_renderer: &TileRenderer) {
     tile_renderer.render_full(renderer, &Point { x: 0, y: 0 }, [1.0, 0.0, 0.0, 1.0]);
     tile_renderer.render_full(renderer, &Point { x: 1, y: 0 }, [0.0, 0.0, 1.0, 1.0]);
 
-    renderer.render_char('#', &Point { x: 0, y: 100 }, 100, [1.0, 1.0, 1.0, 1.0]);
+    renderer.render_char('#', &Point { x: 0, y: 0 }, 100, [1.0, 1.0, 1.0, 1.0]);
 }
--- rusted_arena/src/math/mod.rs
+++ rusted_arena/src/math/mod.rs
@@ -7,5 +7,6 @@ pub struct Point {
 }
 
 impl_op_ex!(+ |a: &Point, b: &Point| -> Point { Point{x: a.x + b.x, y: a.y + b.y}});
+impl_op_ex!(+ |a: &Point, b: &i32| -> Point { Point{x: a.x + b, y: a.y + b}});
 #[rustfmt::skip::macros(impl_op_ex)]
 impl_op_ex!(* |a: &Point, b: &Point| -> Point { Point{x: a.x * b.x, y: a.y * b.y}});
